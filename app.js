284ae7cd02ec5a226de963ab4236c30cc545ae2d
chrismin217 <chris.min216@gmail.com>
2018-05-05 18:24:36 -1000

    Options box, basic script functionality.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hello World');
+const grid = document.getElementById('grid');
+const options = document.getElementById('options');
+const generate = document.getElementById('generate');
+
+function Card(question, answer) {
+  this.question = question;
+  this.answer = answer;
+};
+
+function render(cols, rows) {
+
+  let num = cols * rows;
+
+  for (let i = 0; i < num; i++) {
+  	var flashCard = new Card('English', 'Korean');
+    let cardContainer = document.createElement("div");
+    cardContainer.appendChild(flashCard);
+  	grid.appendChild(cardContainer);
+  }
+  
+
+};
+
+generate.addEventListener("click", render);
+
+
