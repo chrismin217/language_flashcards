7ac1a21dd242ea98e7b90a088d19b07349c156ad
chrismin217 <chris.min216@gmail.com>
2018-05-06 17:30:04 -1000

    Figuring out how to render cards.

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
 const grid = document.getElementById('grid');
 const options = document.getElementById('options');
 const generate = document.getElementById('generate');
+let gridSize = document.getElementById('gridsize').value;
 
 function Card(question, answer) {
   this.question = question;
   this.answer = answer;
 };
 
-function render(cols, rows) {
+/*  The grid is a 600 * 600 px box  */
+function render() {
 
-  let num = cols * rows;
+  console.log('render function');
+  console.log(gridSize);
 
-  for (let i = 0; i < num; i++) {
+  let width = 600 / gridSize;
+  let height = 600 / gridSize;
+
+  for (let i = 0; i < gridSize; i++) {
+
   	var flashCard = new Card('English', 'Korean');
+
     let cardContainer = document.createElement("div");
+    cardContainer.style.width = width;
+    cardContainer.style.height = height;
+    cardContainer.style.border = "1px solid black";
+
     cardContainer.appendChild(flashCard);
   	grid.appendChild(cardContainer);
+
   }
   
 
