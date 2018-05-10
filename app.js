584dc4a0eb0e01c8fef7c26e0fa6c83934bda26b
chrismin217 <chris.min216@gmail.com>
2018-05-10 14:40:47 -0700

    Some refactoring.

--- app.js
+++ app.js
@@ -1,33 +1,43 @@
 const grid = document.getElementById('grid');
 const options = document.getElementById('options');
 const generate = document.getElementById('generate');
-let gridSize = document.getElementById('gridsize').value;
 
 function Card(question, answer) {
   this.question = question;
   this.answer = answer;
 };
 
-/*  The grid is a 600 * 600 px box  */
+
+/*FUNCTIONALITY FOR RENDERING CARDS ON GRID*/
 function render() {
 
-  console.log('render function');
-  console.log(gridSize);
+  let gridSize = document.getElementById('gridsize').value;
+  let wh = 100 / gridSize;
 
-  let width = 600 / gridSize;
-  let height = 600 / gridSize;
-
-  for (let i = 0; i < gridSize; i++) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
 
   	var flashCard = new Card('English', 'Korean');
-
     let cardContainer = document.createElement("div");
-    cardContainer.style.width = width;
-    cardContainer.style.height = height;
-    cardContainer.style.border = "1px solid black";
+    
+    cardContainer.style.width = wh - 5 + '%';
+    cardContainer.style.height = wh - 5 + '%';
+    cardContainer.style.border = "1px solid cyan";
+    cardContainer.style.margin = '2.5%';
+    cardContainer.style.float = 'left';
+    cardContainer.innerHTML = flashCard.question;
+   
+    cardContainer.addEventListener("click", function() {
 
-    cardContainer.appendChild(flashCard);
-  	grid.appendChild(cardContainer);
+      this.innerHTML = flashCard.answer;
+      let timeDifficulty = document.getElementById('time').value;
+
+      setTimeout(() => {
+      	this.innerHTML = flashCard.question; 
+      }, timeDifficulty * 1000);
+
+    });
+
+    grid.appendChild(cardContainer);
 
   }
   
@@ -35,5 +45,14 @@ function render() {
 };
 
 generate.addEventListener("click", render);
+
+/*FUNCTIONALITY FOR RESETTING OPTIONS*/
+const resetButton = document.getElementById("reset");
+
+function resetOptions() {
+  console.log(this);
+};
+
+resetButton.addEventListener("click", resetOptions);
 
 
