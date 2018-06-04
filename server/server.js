41335a0e6821d03e3fb06f5892202b30a2093bde
chrismin217 <chris.min216@gmail.com>
2018-06-03 18:22:34 -1000

    Sequelize init. Server modules.

--- server/server.js
+++ server/server.js
@@ -6,47 +6,116 @@ const path = require('path');
 
 const session = require('express-session');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const expressValidator = require('express-validator');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 const deck = require('./deck');
 
-/*Nodemon is complaining, out for now.*/
+const db = require('../models');
 
-/*app.configure(function() {
-  app.use(express.cookieParser('secret'));
-  app.use(express.session({
-    cookie : { maxAge : 60000 }
-  }));
-  app.use(flash());
-});*/
-
-app.use(bodyParser.urlencoded({ extended:true }));
+/*Body Parser*/
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(cookieParser());
 
+/*Static Folder*/
+app.use(express.static(path.join(__dirname + 'public')));
+
+/*Session*/
+app.use(session({
+  secret : 'secret',
+  saveUninitialized : true,
+  resave : false
+}));
+
+/*Passport initialize*/
+app.use(passport.initialize());
+app.use(passport.session());
+
+/*Validator*/
+app.use(expressValidator({
+  errorFormatter : function(param, msg, value) {
+
+    var namespace = param.split('.');
+    var root = namespace.shift();
+    var formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+
+    return {
+      param : formParam,
+      msg : msg,
+      value : value
+    };
+  }
+
+}));
+
+/*Flash*/
+app.use(flash());
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+/*Solving Error */
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();	
 });
 
+/*Login*/
+app.post('/login', passport.authenticate('local', {
+  failureFlash : 'Invalid username or password.'
+}), (req, res) => {
+  console.log(req.body);
+  res.send('hello');
+});
+
+app.post('/register', (req, res) => {
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+      db.User.create({
+        username : req.body.username,
+        password : hash
+      })
+      .then(user => {
+        res.json(user);
+      })
+      .catch(err => {
+        return res.send('Failed to create new user.');
+      });
+    });
+  });
+});
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.sendStatus(200);
+})
+
+/*Decks*/
+
+
+
+/*Cards*/
 app.get('/api/cards', (req, res) => {
   let random = Math.floor(Math.random() * deck.length);
-  console.log(deck[random]);
   res.json(deck[random]);
 });
 
 app.post('/api/cards', (req, res) => {
   console.log('posting');
-});
-
-app.post('/login', passport.authenticate('local', { 
-  failureFlash : 'Invalid username or password.'
-}), (req, res) => {
-  res.redirect();
 });
 
 
@@ -59,9 +128,8 @@ app.delete('/api/cards/:id', (req, res) => {
 });
 
 
-
-
-
+/*Server*/
 app.listen(PORT, () => {
+  db.sequelize.sync({ force : false });
   console.log(`Listening on port: ${PORT}`);
 });
