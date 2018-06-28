fa6cbfd49feeee2bf59d5a7a07f1e327b76f911d
chrismin217 <chris.min216@gmail.com>
2018-06-27 16:31:39 -1000

    maxlength 20 to add deck, styled deck icons.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -11,19 +11,34 @@ decksReq.addEventListener("load", function() {
 	const decksList = document.getElementById("decks-list");
 
 	for (let i = 0; i < usersDecks.length; i++) {
+
 	  let userDeck = document.createElement("li");
 	  let userDeckButton = document.createElement("a");
+	  let userDeckIcon = document.createElement("img");
+	  let userDeckLabel = document.createElement("span");
+
+	  userDeckIcon.src = '/assets/dashboard/black_deck.png';
+	  userDeckIcon.alt = '';
 
 	  userDeck.classList.add("user-deck");
 	  userDeckButton.classList.add("user-deck-button");
-	  userDeckButton.onclick = function(e) {
-	  	console.log('deck clicked.'); //this should be an XHR
-	  };
+	  userDeckIcon.classList.add("user-deck-icon");
+	  userDeckLabel.classList.add("user-deck-label");
 
-	  userDeckButton.innerHTML = usersDecks[i].title;
-
+	  userDeckButton.appendChild(userDeckLabel);
+	  userDeckButton.appendChild(userDeckIcon);
 	  userDeck.appendChild(userDeckButton);
 	  decksList.appendChild(userDeck);	
+
+	  userDeckLabel.innerHTML = usersDecks[i].title;
+
+	  userDeckButton.onclick = function(e) {
+	  	//XHR
+	  	console.log('deck clicked.');
+	  };
+
+
+
 	}
 
 });
@@ -67,9 +82,9 @@ function deckSubmit(formElement) {
 /*For Delete Decks*/
 function deleteDecks() {
 	console.log('deleting decks.');
-}
+};
 
 /*For Edit Decks*/
 function editDecks() {
-	console.log('editting decks.');
-}
+	alert('Edit Decks feature coming soon!');
+};
